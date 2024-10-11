298e43dba5ed632c1634a046ba0bdc34eafcee23
Abdoul03 <samabdoul03@gmail.com>
2024-10-11 00:17:48 +0000

    first commit

---
+++ abonnCanal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AbonncanalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'abonnCanal'
---
+++ mobileMonneyTrans/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MobilemonneytransConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mobileMonneyTrans'
